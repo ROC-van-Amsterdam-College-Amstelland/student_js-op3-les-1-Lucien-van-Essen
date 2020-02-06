780258857fdf998abc8c7185b80b75655bf13079
Lucien van Essen <2113629@talnet.nl>
2020-02-06 10:39:15 +0100

    opdracht 7 KLAAR!

--- opdracht07/js/script.js
+++ opdracht07/js/script.js
@@ -11,7 +11,7 @@ var lichtRood = "red";
 var lichtOranje = "orange";
 var lichtGroen = "green";
 
-window.onload = function(){
+window.onload = function () {
     document.getElementById("left-light-top").innerHTML = stop;
     document.getElementById("left-light-middle").innerHTML = maybe;
     document.getElementById("left-light-bottom").innerHTML = go;
@@ -24,50 +24,54 @@ window.onload = function(){
     document.getElementById("right-stoplight-label").innerHTML = "B";
 }
 
-function verkeerMagRijdenStoplichtB(booleanVerkeerMagRijden){
-    if(booleanVerkeerMagRijden == true){
+function verkeerMagRijdenStoplichtB(booleanVerkeerMagRijden) {
+    if (booleanVerkeerMagRijden == true) {
         document.getElementById("right-light-bottom").style.backgroundColor = lichtGroen;
         document.getElementById("right-light-top").style.backgroundColor = lichtUit;
     }
-    else{
+    else {
         document.getElementById("right-light-bottom").style.backgroundColor = lichtUit;
         document.getElementById("right-light-top").style.backgroundColor = lichtRood;
     }
 }
 
-function toggleRoodLicht(){
-    if(roodLichtStaatAan == false){
+function toggleRoodLicht() {
+    if (roodLichtStaatAan == false) {
         document.getElementById("left-light-top").style.backgroundColor = "red";
         roodLichtStaatAan = true;
         //JOUW CODE HIER
+        document.getElementById("right-light-bottom").style.backgroundColor = "green";
     }
-    else{
+    else {
         document.getElementById("left-light-top").style.backgroundColor = "gray";
         roodLichtStaatAan = false;
         //JOUW CODE HIER
+        document.getElementById("right-light-bottom").style.backgroundColor = "gray";
     }
 }
 
-function toggleOranjeLicht(){
-    if(oranjeLichtStaatAan == false){
+function toggleOranjeLicht() {
+    if (oranjeLichtStaatAan == false) {
         document.getElementById("left-light-middle").style.backgroundColor = "orange";
         oranjeLichtStaatAan = true;
     }
-    else{
+    else {
         document.getElementById("left-light-middle").style.backgroundColor = "gray";
         oranjeLichtStaatAan = false;
     }
 }
 
-function toggleGroenLicht(){
-    if(groenLichtStaatAan == false){
+function toggleGroenLicht() {
+    if (groenLichtStaatAan == false) {
         document.getElementById("left-light-bottom").style.backgroundColor = "green";
         groenLichtStaatAan = true;
         //JOUW CODE HIER
+        document.getElementById("right-light-top").style.backgroundColor = "red";
     }
-    else{
+    else {
         document.getElementById("left-light-bottom").style.backgroundColor = "gray";
         groenLichtStaatAan = false;
         //JOUW CODE HIER
+        document.getElementById("right-light-top").style.backgroundColor = "gray";
     }
 }
